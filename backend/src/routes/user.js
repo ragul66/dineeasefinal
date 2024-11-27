9ad4722db911abccca992529dd0b0b4f2b540565
Ragul Vasanth.S.M <114981373+ragul66@users.noreply.github.com>
2024-11-27 11:48:25 +0530

    Update user.js

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -73,7 +73,7 @@ router.post("/userlogin", async (req, res) => {
 
     if (user.password) {
       const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
+      if (isMatch) {
         return res.status(400).json({ message: "Invalid credentials" });
       }
     } else {
